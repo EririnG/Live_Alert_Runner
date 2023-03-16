8cc756408fcb6c9dc17892956c0fef3c01868785
EririnG <qwey1584@gmail.com>
2023-03-16 14:54:56 +0900

    Parallax Scrolling

--- Assets/Script/Scrolling.cs
+++ Assets/Script/Scrolling.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Scrolling : MonoBehaviour
 {
-
+    public float ob = 1;
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(-5.0f,0.0f,0.0f)*Time.deltaTime ;
+        transform.position += new Vector3(1.0f,0.0f,0.0f)*Time.deltaTime*ob ;
     }
 }
