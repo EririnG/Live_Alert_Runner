275c2c2ba1371a14196d577ec9df0d4f07f00780
EririnG <qwey1584@gmail.com>
2023-03-17 19:12:24 +0900

    Score

--- Assets/Script/ScoreScript.cs
+++ Assets/Script/ScoreScript.cs
@@ -8,19 +8,22 @@ using UnityEngine.UI;
 public class ScoreScript : MonoBehaviour
 {
     public float GameTime = 0;
-    public int Score;
-    private Text myScore;
+    public int iScore;
+    public Text Score;
+    Text a;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Score = GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
         GameTime += Time.deltaTime * 5.0f;
-        Score = (int)GameTime;
+        iScore = (int)GameTime;
+        a.text = Score.ToString();
+        String.Format("{0:0000}",a);
     }
 }
