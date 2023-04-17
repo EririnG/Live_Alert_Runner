4187ad0b998a2995d0f1dfe6df3676f03261c927
EririnG <qwey1584@gmail.com>
2023-04-17 15:00:31 +0900

    move walk animation, gold bar

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -7,11 +7,12 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public GameObject imageRestart;
-
+    public Vector3[] PointList;
     // Start is called before the first frame update
     void Start()
     {
-        imageRestart.SetActive(false);   
+        if(imageRestart != null)
+            imageRestart.SetActive(false);   
     }
 
     // Update is called once per frame
