c380084c77058132ede2581d59cbc935e133248f
EririnG <qwey1584@gmail.com>
2023-05-31 14:26:34 +0900

    캐릭터 구매 _ 같은 스프라이트 오류

---
+++ Assets/Script/TitlePlayer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitlePlayer
+{
+    public int ID;
+
+    public TitlePlayer(int id)
+    { 
+        this.ID = id;
+    }
+
+}
